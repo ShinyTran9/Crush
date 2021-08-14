1c8aae667c667631036057cbe4e60daf292952c7
Your Name <you@example.com>
2021-08-14 17:55:25 +0700

    bo sung them

--- js/myJs.js
+++ js/myJs.js
@@ -115,7 +115,7 @@ $(document).ready(function () {
       width: 900,
       padding: "3em",
       html: "<input type='text' class='form-control' id='txtReason'  placeholder='Whyyyyyyyyyyyyy'>",
-      background: '#fff url("img/iput-bg.jpg")',
+      background: '#fff url("img/input-bg.jpg")',
       backdrop: `
                     rgba(0,0,123,0.4)
                     url("img/giphy2.gif")
